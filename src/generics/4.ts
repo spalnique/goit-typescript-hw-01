8d767f558aaf5a875aaf3a871494007a2f8b3ba2
spalnique <136997639+spalnique@users.noreply.github.com>
2024-05-06 09:32:38 +0200

    Update 4.ts

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -9,7 +9,15 @@ function createOrUpdateUser(initialValues: Partial<UserX>): UserX {
   return { name: 'hello', surname: 'world', email: 'a@a.a', password: 'a' };
 }
 
-createOrUpdateUser({
+// або так:
+
+function createOrUpdateUser<T>(initialValues: T, newValues: Partial<T>): T {
+  return { ...initialValues, ...newValues };
+}
+
+const testUser: UserX = { name: 'hello', surname: 'world', email: 'a@a.a', password: 'a' };
+
+createOrUpdateUser<UserX>(testUser, {
   email: 'user@mail.com',
   password: 'password123',
 });
